94311682da2fde8c3082dab3465406b0924811ae
jmphilips <jmichaelphilips@gmail.com>
2018-08-30 21:25:15 -0400

    Initial commit.

---
+++ app/controllers/bands_controller.rb
@@ -0,0 +1,32 @@
+class BandsController < ApplicationController
+  def index
+    @bands = Band.all
+  end
+
+  def show
+    get_band
+  end
+
+  def new
+    @band = Band.new
+  end
+
+  def create
+    create_band(params)
+    @bands = Band.all
+    render action: "index"
+  end
+
+  private
+
+  def get_band
+    @band = Band.find(params[:id])
+  end
+
+  def create_band(params)
+    band_data = params[:band]
+    Band.create!(
+      name: band_data[:name]
+    )
+  end
+end
