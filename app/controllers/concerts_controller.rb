9b23a2e4067154b205ccaad2dd5cee789a62ab80
jmphilips <jmichaelphilips@gmail.com>
2018-09-08 18:45:44 -0400

    Updated the views and add concert/add band button.

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -7,11 +7,19 @@ class ConcertsController < ApplicationController
     @concert = Concert.new
   end
 
+  def show
+    get_concert
+  end
+
   def create
     create_concert(params)
   end
 
   private
+
+  def get_concert
+    @concert = Concert.find(params[:id])
+  end
 
   def create_concert(params)
     concert_data = params[:concert]
